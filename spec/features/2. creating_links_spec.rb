c2d15ef5eb91ab13cb8df9cd22a0cdf73cf8165b
Pedro Castanheira <josepedrocastanheira@gmail.com>
2017-01-26 08:53:34 +0000

    Heroku config - Procfile added

--- spec/features/2. creating_links_spec.rb
+++ spec/features/2. creating_links_spec.rb
@@ -1,19 +1,10 @@
-# require 'database_cleaner'
-
-# DatabaseCleaner.strategy = :transaction
-#
-# DatabaseCleaner.cleaning do
-
 feature '2. add a link' do
   scenario 'add the site address and title to my bookmark manager' do
-    # DatabaseCleaner.strategy = :transaction
-    # DatabaseCleaner.cleaning do
     visit '/links'
     click_button('Add Link')
     fill_in(:title, with: 'Facebook')
     fill_in(:url, with: 'www.facebook.com')
     click_button('Save')
     expect(page).to have_text('Facebook')
-    end
-  # end
+  end
 end
