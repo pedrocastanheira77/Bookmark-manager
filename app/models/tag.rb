5ee5953224ee16b470b1d8a114c6da54059f8694
Pedro Castanheira <josepedrocastanheira@gmail.com>
2017-01-26 16:30:40 +0000

    Filtering by tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,9 @@ require 'data_mapper'
 
 class Tag
   include DataMapper::Resource
-  
+
+  has n,   :links, through: Resource
+
   property :id,    Serial
   property :name, String
 end
---
+++ spec/features/4. filter_by_test_spec.rb
@@ -0,0 +1,25 @@
+# As a time-pressed user
+# So that I can quickly find links on a particular topic
+# I would like to filter links by tag
+
+feature '4. filter links by tag' do
+  before(:each) do
+    Link.create(
+    title: 'Facebook',
+    url: 'www.facebook.com',
+    tags: [Tag.first_or_create(name: "social")])
+    Link.create(
+    title: 'Facebook',
+    url: 'www.facebook.com',
+    tags: [Tag.first_or_create(name: "bubbles")])
+  end
+
+  scenario 'filter by tag' do
+    visit '/tags/bubbles'
+    expect(page.status_code).to eq(200)
+    within("ul#links") do
+      expect(page).to have_content('bubbles')
+      expect(page).not_to have_content('social')
+    end
+  end
+end
