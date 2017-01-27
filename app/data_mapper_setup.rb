7942bb91634e6ee8481b1394e58f96ba01a80854
Pedro Castanheira <josepedrocastanheira@gmail.com>
2017-01-27 09:54:18 +0000

    changed data_mapper_setup file

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,4 +4,4 @@ require_relative 'models/tag'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/book_manager_#{ENV["RACK_ENV"]}")
 DataMapper.finalize
-DataMapper.auto_migrate!
+DataMapper.auto_upgrade!
